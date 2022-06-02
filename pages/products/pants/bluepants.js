5853352ea6191db806aa58845e3c4e10ef61d069
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-02 16:19:49 -0300

    Nested Routes

---
+++ pages/products/pants/bluepants.js
@@ -0,0 +1,3 @@
+export default function BluePants() {
+    return <h1>Blue Pants - R$ 189,98</h1>
+}
---
+++ pages/products/pants/redpants.js
@@ -0,0 +1,3 @@
+export default function RedPants() {
+    return <h1>Red Pants - R$176,99</h1>
+}
