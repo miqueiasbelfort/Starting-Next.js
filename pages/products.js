db09b6fd98c0811b0ba248df775af668b42098cb
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-02 15:58:04 -0300

    pages

---
+++ pages/products.js
@@ -0,0 +1,15 @@
+export default function Products() {
+    return (
+        <div>
+            <h1>Produtos</h1>
+            <div>
+                <h2>Celular</h2>
+                <p>R$ 1.515,99</p>
+            </div>
+            <div>
+                <h2>Notbook</h2>
+                <p>R$ 3.798,99</p>
+            </div>
+        </div>
+    )
+}
