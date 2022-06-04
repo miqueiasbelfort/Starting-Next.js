cbd4a96a884c3eb668e845126c971f490a546040
miqueiasbelfort <kawasousa1554@gmail.com>
2022-06-04 16:08:39 -0300

    assets

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import styles from '../styles/Home.module.css'
 
 import Head from "next/head"
+import Image from 'next/image'
 
 export default function Home() {
 
@@ -13,6 +14,12 @@ export default function Home() {
       </Head>
       <div>
         <h1 className={styles.title}>Hello World Next.js</h1>
+        <Image
+          src="/images/girl.jpg"
+          width="220px"
+          height="400px"
+          alt="lofi girl"
+        />
       </div>
     </>
   )
